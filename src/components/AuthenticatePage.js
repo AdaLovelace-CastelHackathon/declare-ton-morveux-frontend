29a67f9109f84612120d8b009a4c4148eafe0126
gitSushi <lossushi37@gmail.com>
2021-05-07 07:26:12 +0200

    nav solved

--- src/components/AuthenticatePage.js
+++ src/components/AuthenticatePage.js
@@ -42,7 +42,9 @@ function AuthenticatePage({ setHasDeclared, hasDeclared, setIsAuth }) {
         () => console.log("already taken")
       )
       .catch((error) => alert(`Sorry, there was an error`));
-    isAuthenticated().then((response) => setIsAuth(response.data));
+    isAuthenticated()
+      .then((response) => response)
+      .then((data) => setIsAuth(data));
   };
 
   const handleSubmitLogin = (e) => {
@@ -56,7 +58,9 @@ function AuthenticatePage({ setHasDeclared, hasDeclared, setIsAuth }) {
         alert(`Sorry, there was an error`);
         console.log(error);
       });
-    isAuthenticated().then((response) => setIsAuth(response.data));
+    isAuthenticated()
+      .then((response) => response)
+      .then((data) => setIsAuth(data));
   };
 
   const signSpec = {
